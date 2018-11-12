7b4209394b25b834f96075e158505d7eeb47120b
Nikita Timofeev <hik951@gmail.com>
2018-11-12 13:48:52 +0200

    Add files via upload

---
+++ km-52/Tymofeiev_Nikita/workshop1/sql/3task.sql
@@ -0,0 +1,10 @@
+select*
+from student;
+
+delete from result_ where group_id = 'KM-52';
+delete from student where group_id = 'KM-52';
+delete from student_group where group_id = 'KM-52';
+select *
+from  result_;
+select *
+from student_group;
